282f9da1b16b83e3756b7ef913f43de310b617b9
Exyde <54008677+Exyde@users.noreply.github.com>
2021-02-23 20:18:38 +0100

    Player Movement
    Added Walk, Run, Camera.
    Need to Fix Crouch (Refacto)
    Added placeholder Hands Model.

---
+++ Assets/Scripts/Crouch.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (FPSController))]
+public class Crouch : MonoBehaviour
+{
+    FPSController controller;
+    CharacterController cc;
+
+    public KeyCode CrounchKey = KeyCode.C;
+    public bool crouched = false;
+
+    float startSize;
+    public float crounchSize;
+
+    void Start()
+    {
+        controller = GetComponent<FPSController>();
+        cc = GetComponent<CharacterController>();
+        startSize = cc.height;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(CrounchKey))
+		{
+            crouched = !crouched;
+            CheckCrouch();
+		}
+    }
+
+    void CheckCrouch()
+	{
+        cc.height = crouched ? startSize / 2 : startSize;
+	}
+
+}
---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,91 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (CharacterController))]
+public class FPSController : MonoBehaviour
+{
+    #region Public Fields
+
+    [Header("Inputs")]
+    public KeyCode RunKey = KeyCode.LeftShift;
+
+    [Header ("Speeds")]
+    [Range (5, 10)]
+    public float walkingSpeed = 7.5f;
+    [Range(8, 20)]
+    public float runningSpeed = 11.5f;
+    [Range (6, 10)]
+    public float jumpSpeed = 8.0f;
+
+    [Space]
+    public float gravity = 20.0f;
+
+    [Header ("Camera")]
+    public Camera playerCamera;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+
+    [Header ("Debug")]
+    //[HideInInspector]
+    public bool canMove = true;
+    public bool showCursor = true;
+    #endregion
+
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        //Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = showCursor;
+    }
+
+    void Update()
+    {
+        // We are grounded, so recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(RunKey);
+
+        //Compute Speed
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpSpeed;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        //If not grounded, apply gravity
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        // Player and Camera rotation
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+    }
+}
+
