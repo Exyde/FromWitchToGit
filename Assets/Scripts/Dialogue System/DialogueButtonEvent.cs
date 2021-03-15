baf6d28704516fb927ce31218a928895abc4c035
Exyde <54008677+Exyde@users.noreply.github.com>
2021-03-15 10:40:28 +0100

    Update Commit From Uni

--- Assets/Scripts/Dialogue System/DialogueButtonEvent.cs
+++ Assets/Scripts/Dialogue System/DialogueButtonEvent.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DialogueButtonEvent : MonoBehaviour, IPointerClickHandler
+public class DialogueButtonEvent : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
 	#region Public Fields
 	public UnityEvent onClick;
@@ -20,11 +21,31 @@ public class DialogueButtonEvent : MonoBehaviour, IPointerClickHandler
 	private void Start()
 	{
 		dialogueManager = GetComponentInParent<DialogueManager>();
-	}
+
+        Color c = GetComponent<Text>().color;
+        c.a = .7f;
+        GetComponent<Text>().color = c;
+    }
 
 	public void OnPointerClick(PointerEventData pointerEventData)
     {
         onClick.Invoke();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Color c = GetComponent<Text>().color;
+        c.a = 1f;
+
+        GetComponent<Text>().color = c;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Color c = GetComponent<Text>().color;
+        c.a = .2f;
+
+        GetComponent<Text>().color = c;
     }
 
     public void SetNextMessage()
