80b6fc196b8518bb705043cae8a7afc2ad7f34fe
Exyde <54008677+Exyde@users.noreply.github.com>
2021-03-17 15:46:15 +0100

    Voiceslines Script on PNJ

--- Assets/Scripts/Events/OnTriggerVoiceLine.cs
+++ Assets/Scripts/Events/OnTriggerVoiceLine.cs
@@ -9,18 +9,46 @@ public class OnTriggerVoiceLine : MonoBehaviour
 	bool hasPlayed = false;
 	AudioSource source;
 
+	public bool replayable = false;
+	public float cooldown = 5f;
+	float timer = 0;
+
 	private void Start()
 	{
 		hasPlayed = false;
 		source = GetComponent<AudioSource>();
 		source.clip = clip;
 	}
-	void OnTriggerEnter(Collider other)
+
+    private void Update()
+    {
+        if (replayable)
+        {
+			if (timer > 0 && hasPlayed)
+            {
+				timer -= Time.deltaTime;
+            }
+
+			if (timer <= 0)
+            {
+				hasPlayed = false;
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Player" && !hasPlayed)
 		{
 			source.Play();
 			hasPlayed = true;
+
+			if (!replayable)
+            {
+				Destroy(this);
+            } else
+            {
+				timer = cooldown;
+            }
 		}
 	}
 }
